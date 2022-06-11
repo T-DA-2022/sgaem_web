0ab52a38093bf011b3dec45ca75fd7409a193a31
moong <moonki0623@naver.com>
2022-06-11 18:59:33 +0900

    [feat]: mainpage indexing completed

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,4 +1,10 @@
 import React from "react";
+import img1 from "../assets/car_img_1.jpeg";
+import img2 from "../assets/car_img_2.jpeg";
+import img3 from "../assets/car_img_3.jpeg";
+import img4 from "../assets/car_img_4.jpeg";
+
+// style import
 import {
   MainpageContainer,
   CarouselContainer,
@@ -16,6 +22,7 @@ const Mainpage = () => {
           So Gang Association of E-sports Management
         </CarouselContainer>
         <VisionContainer>
+          <img src="../assets/car_img_1.jpeg" alt="" />
           Vision Vision Vision Vision Vision Vision
         </VisionContainer>
         <ActivityContainer>
