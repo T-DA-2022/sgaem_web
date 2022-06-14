d307b09940a9676970be26e0388069a6f4cadd9b
moong <moonki0623@naver.com>
2022-06-15 04:05:07 +0900

    organizing

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import img1 from "../assets/car_img_1_crop.png";
-import img2 from "../assets/car_img_2.jpeg";
-import img3 from "../assets/car_img_3.jpeg";
+// import img2 from "../assets/car_img_2.jpeg";
+// import img3 from "../assets/car_img_3.jpeg";
 // import img4 from "../assets/car_img_4.jpeg";
 
 // style import
