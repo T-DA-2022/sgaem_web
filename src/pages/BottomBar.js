8a23f75851bbcbc192585524e0bbaf7d8c238af4
moong <moonki0623@naver.com>
2022-06-26 19:03:35 +0900

    bottombar temp_fix needed

--- src/pages/BottomBar.js
+++ src/pages/BottomBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import {
   BottomBarBox,
   BottomBarBoxText,
+  BottomBarButtonContainer,
   BottomBarContainer,
   BottomBarIndex,
 } from "../styles/BottomBar.element";
@@ -13,10 +14,12 @@ const BottomBar = () => {
         <BottomBarIndex>
           <BottomBarBox>
             <BottomBarBoxText>
-              주소 : 서강대학교 엠마오관 E132호 <br />
+              주소 : 서울특별시 마포구 백범로 35 서강대학교 엠마오관 E132호
+              <br />
               이메일 : sogangsgaem@gmail.com
             </BottomBarBoxText>
           </BottomBarBox>
+          <BottomBarButtonContainer>asdfasdf</BottomBarButtonContainer>
         </BottomBarIndex>
       </BottomBarContainer>
     </>
--- src/styles/BottomBar.element.js
+++ src/styles/BottomBar.element.js
@@ -1,29 +1,37 @@
 import styled from "styled-components";
 
 export const BottomBarContainer = styled.div`
-  height: 250px;
+  height: 150px;
   width: 100%;
   border-top: 1px solid #707070;
-  background-color: #e9e9e9;
+  background-color: #181830;
   display: flex;
   justify-content: center;
 `;
 
 export const BottomBarIndex = styled.div`
   width: 62.5%;
-  margin-top: 60px;
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 `;
 
 export const BottomBarBox = styled.div`
-  height: 70px;
-  width: 300px;
+  height: max-content;
+  width: max-content;
   border-top: 1px solid #707070;
   border-bottom: 1px solid #707070;
 `;
 
 export const BottomBarBoxText = styled.div`
-  padding-left: 20px;
-  padding-top: 10px;
+  padding: 10px 10px;
   font-size: 16px;
-  color: #707070;
+  color: #ffffff;
+`;
+
+export const BottomBarButtonContainer = styled.div`
+  background-color: pink;
+  width: 100px;
+  height: max-content;
 `;
