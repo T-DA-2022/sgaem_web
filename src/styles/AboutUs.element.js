084dcc1a144982335e0b474c77d92070f54ead89
moong <moonki0623@naver.com>
2022-06-30 20:39:55 +0900

    about us css file

---
+++ src/styles/AboutUs.element.js
@@ -0,0 +1,135 @@
+import styled from "styled-components";
+
+export const AboutUsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const BannerContainer = styled.div`
+  background-color: #f5f5f5;
+  font-size: 56px;
+  height: 20vw;
+  display: flex;
+  width: 100vw;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TabArea = styled.div`
+  height: 11vw;
+  background-color: #ffffff;
+  display: flex;
+  justify-content: center;
+`;
+
+export const TabBox = styled.div`
+  background-color: #ffffff;
+  box-shadow: 0px 4px 5px #00000044;
+  height: 90px;
+  width: 62.5vw;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TabContent = styled.div`
+  /* background-color: pink; */
+  display: flex;
+  margin: 0 25px;
+  height: 100%;
+  align-items: center;
+  font-size: 18px;
+`;
+
+export const VisionContainer = styled.div`
+  background-color: #f8f8fa;
+  padding-top: 6.25vw;
+  padding-bottom: 6.25vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const VisionTextContainer = styled.div`
+  /* margin-top: 90px; */
+  margin-bottom: 8px;
+  color: #707070;
+  font-size: 40px;
+  font-weight: bold;
+`;
+
+export const VisionSubTextContainer = styled.div`
+  font-size: 18px;
+  color: #707070;
+  margin-bottom: 4.7vw;
+`;
+
+export const HistoryContainer = styled.div`
+  background-color: #f1f1f5;
+  padding-top: 6.25vw;
+  padding-bottom: 6.25vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const HistoryTextContainer = styled.div`
+  /* margin-top: 90px; */
+  margin-bottom: 8px;
+  color: #707070;
+  font-size: 40px;
+  font-weight: bold;
+`;
+
+export const HistorySubTextContainer = styled.div`
+  font-size: 18px;
+  color: #707070;
+  margin-bottom: 4.7vw;
+`;
+
+export const ActivityContainer = styled.div`
+  background-color: #f8f8fa;
+  padding-top: 6.25vw;
+  padding-bottom: 6.25vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ActivityTextContainer = styled.div`
+  /* margin-top: 90px; */
+  margin-bottom: 8px;
+  color: #707070;
+  font-size: 40px;
+  font-weight: bold;
+`;
+
+export const ActivitySubTextContainer = styled.div`
+  font-size: 18px;
+  color: #707070;
+  margin-bottom: 4.7vw;
+`;
+
+export const TeamContainer = styled.div`
+  background-color: #f1f1f5;
+  padding-top: 6.25vw;
+  padding-bottom: 6.25vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const TeamTextContainer = styled.div`
+  /* margin-top: 90px; */
+  margin-bottom: 8px;
+  color: #707070;
+  font-size: 40px;
+  font-weight: bold;
+`;
+
+export const TeamSubTextContainer = styled.div`
+  font-size: 18px;
+  color: #707070;
+  margin-bottom: 4.7vw;
+`;
