8990c81dd8208f2cf8335cdb4af221e9424d80c5
moong <moonki0623@naver.com>
2022-07-06 13:10:27 +0900

    login page initialize

---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { LoginContainer, LoginText } from "../styles/Login.element";
+
+const Login = () => {
+  return (
+    <>
+      <LoginContainer>
+        <LoginText>MEMBER LOGIN</LoginText>
+      </LoginContainer>
+    </>
+  );
+};
+export default Login;
---
+++ src/styles/Login.element.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const LoginContainer = styled.div`
+  /* background-color: #f9fafb; */
+  background-color: skyblue;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const LoginText = styled.div`
+  background-color: pink;
+  font-size: 40px;
+  font-weight: bold;
+`;
