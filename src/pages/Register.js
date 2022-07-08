f05e202d095c825874f9254d88db84a9d2a8157c
moong <moonki0623@naver.com>
2022-07-08 15:05:33 +0900

    register page input box

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,55 @@
 import React from "react";
+import {
+  FormContainer,
+  FormInput,
+  FormRadio,
+  FormText,
+  JoinText,
+  RegisterContainer,
+} from "../styles/Register.element";
 
 const Register = () => {
   return (
     <>
-      <>register</>
+      <RegisterContainer>
+        <JoinText>JOIN</JoinText>
+        <FormContainer>
+          <FormText>아이디</FormText>
+          <FormInput type="text" placeholder="sogangsgaem@gmail.com" />
+          <FormText>비밀번호</FormText>
+          <FormInput
+            type="password"
+            placeholder="8자 이상, 대문자 + 숫자 조합"
+          />
+          <FormText>비밀번호 확인</FormText>
+          <FormInput type="password" />
+          <FormText>이름</FormText>
+          <FormInput type="text" placeholder="김서강" />
+          <FormText>학번</FormText>
+          <FormInput type="text" placeholder="20181234" />
+          <FormText>생년월일</FormText>
+          <FormInput type="text" placeholder="생년월일 8자리 (ex.19980623)" />
+          <FormText>성별</FormText>
+          <FormRadio>
+            <div>
+              <input type="radio" id="man" name="gender" value="man" />
+              <label for="man">남</label>
+            </div>
+            <div>
+              <input type="radio" id="woman" name="gender" value="woman" />
+              <label for="woman">여</label>
+            </div>
+            <div>
+              <input type="radio" id="etc" name="gender" value="etc" />
+              <label for="etc">기타</label>
+            </div>
+          </FormRadio>
+          <FormText>본인 확인 이메일 (선택)</FormText>
+          <FormInput type="text" placeholder="" />
+          <FormText>휴대전화번호</FormText>
+          <FormInput type="text" placeholder="010-1234-5678" />
+        </FormContainer>
+      </RegisterContainer>
     </>
   );
 };
---
+++ src/styles/Register.element.js
@@ -0,0 +1,74 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const RegisterContainer = styled.div`
+  background-color: #f9fafb;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const JoinText = styled.div`
+  /* background-color: pink; */
+  margin-top: 3.645vw;
+  font-size: 40px;
+  font-weight: bold;
+  @media only screen and (max-width: 1440px) {
+    font-size: 32px;
+  }
+`;
+
+export const FormContainer = styled.div`
+  /* background-color: green; */
+  /* width: 400px; */
+  margin-top: 3.8vw;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  @media only screen and (max-width: 1440px) {
+    /* width: 360px; */
+    margin-top: 3.645vw;
+  }
+`;
+
+export const FormText = styled.div`
+  font-size: 16px;
+  font-weight: bold;
+  margin-bottom: 8px;
+  padding-left: 4px;
+  align-self: flex-start;
+  @media only screen and (max-width: 1440px) {
+    font-size: 12px;
+    margin-bottom: 6px;
+  }
+`;
+
+export const FormInput = styled.input`
+  width: 448px;
+  height: 40px;
+  border-radius: 10px;
+  border: 1px solid #707070;
+  padding: 4px 16px;
+  margin-bottom: 30px;
+  font-size: 16px;
+  @media only screen and (max-width: 1440px) {
+    width: 326px;
+    height: 30px;
+    margin-bottom: 20px;
+  }
+`;
+
+export const FormRadio = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 80%;
+  justify-content: space-between;
+  margin-bottom: 30px;
+  height: 40px;
+  align-items: center;
+  @media only screen and (max-width: 1440px) {
+    height: 30px;
+    margin-bottom: 20px;
+  }
+`;
