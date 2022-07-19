99e49ad5b7860300d2f4c5fc997cd0cd840dc734
moong <moonki0623@naver.com>
2022-07-19 20:20:18 +0900

    faq init

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -1,10 +1,24 @@
 import React from "react";
+import img_bg_src from "../assets/car_img_1_crop_4.png";
+
+import {
+  FAQBackgroundContainer,
+  FAQBackgroundText,
+  FAQBackgroundTextContainer,
+  FAQContainer,
+  FAQContentContainer,
+} from "../styles/FAQ.element";
 
 const FAQ = () => {
   return (
-    <>
-      <div>hello</div>
-    </>
+    <FAQContainer>
+      <FAQBackgroundContainer>
+        <FAQBackgroundTextContainer>
+          <FAQBackgroundText>FAQ</FAQBackgroundText>
+        </FAQBackgroundTextContainer>
+      </FAQBackgroundContainer>
+      <FAQContentContainer>asdf</FAQContentContainer>
+    </FAQContainer>
   );
 };
 
--- src/styles/FAQ.element.js
+++ src/styles/FAQ.element.js
@@ -1,1 +1,41 @@
 import styled from "styled-components";
+
+export const FAQContainer = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const FAQBackgroundContainer = styled.div`
+  background-image: url(${(props) => props.src});
+  width: 100vw;
+  height: 21vw;
+  object-fit: none;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+`;
+
+export const FAQBackgroundTextContainer = styled.div`
+  /* background-color: yellow; */
+  width: 100vw;
+  background-color: #f5f5f5;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const FAQBackgroundText = styled.div`
+  font-size: 56px;
+  color: black;
+  @media only screen and (max-width: 1440px) {
+    font-size: 44px;
+  }
+`;
+
+export const FAQContentContainer = styled.div`
+  background-color: blue;
+  width: 100vw;
+`;
