1edba1f3d983c072576ad9bfec390e56b09a2a78
moong <moonki0623@naver.com>
2023-03-03 10:07:49 +0900

    sitemap added

---
+++ src/sitemapRoutes.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router";
+
+export default (
+  <Routes>
+    <Route path="/" />
+    <Route path="aboutus" />
+    <Route path="news" />
+    <Route path="course" />
+    <Route path="competition" />
+    <Route path="recruiting" />
+    <Route path="faq" />
+    <Route path="contactus" />
+    <Route path="login" />
+    <Route path="register" />
+    <Route path="mypage" />
+    <Route path="fixmypage" />
+    <Route path="logout" />
+    <Route />
+  </Routes>
+);
